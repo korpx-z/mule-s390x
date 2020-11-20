d07d211471374c06ade949fb502ebf580911fe5a
Charles Korpics <72992136+korpx-z@users.noreply.github.com>
2020-11-20 14:42:55 -0500

    Create test.sh

---
+++ test.sh
@@ -0,0 +1,15 @@
+
+set -e
+
+export ANSI_YELLOW="\e[1;33m"
+export ANSI_GREEN="\e[32m"
+export ANSI_RESET="\e[0m"
+
+echo -e "\n $ANSI_YELLOW *** testing docker run - mule  *** $ANSI_RESET \n"
+
+echo -e "$ANSI_YELLOW is mule listening/running?: $ANSI_RESET"
+
+docker run --name Mule -d -p 8081:7777  quay.io/ibmz/mule:3.9.1 mule start
+docker logs Mule 
+
+echo -e "\n $ANSI_GREEN *** TEST COMPLETED SUCESSFULLY *** $ANSI_RESET \n"
