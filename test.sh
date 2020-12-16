34e2cca7ab8972ff23f88ed41347ff54ee434472
Charles Korpics <72992136+korpx-z@users.noreply.github.com>
2020-12-16 15:07:31 -0500

    Update test.sh

--- test.sh
+++ test.sh
@@ -9,7 +9,7 @@ echo -e "\n $ANSI_YELLOW *** testing docker run - mule  *** $ANSI_RESET \n"
 
 echo -e "$ANSI_YELLOW is mule listening/running?: $ANSI_RESET"
 
-docker run --name Mule -d -p 8081:7777  quay.io/ibmz/mule:3.9.1 mule start
+docker run --name Mule -d -p 8081:7777  quay.io/ibm/mule:3.9.1 mule start
 docker logs Mule 
 
 echo -e "\n $ANSI_GREEN *** TEST COMPLETED SUCESSFULLY *** $ANSI_RESET \n"
